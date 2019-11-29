836c3af2d4899ec86eb71725b771b5822ab9eac4
Matthew Tran <mtran014@uottawa.ca>
2019-11-29 00:04:00 -0800

    start game

--- src/projetCSI2772.cpp
+++ src/projetCSI2772.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(){
-
+    /* Testing methods
     Vitrail* vit=new Vitrail(7,5);
     cout <<vit<<endl;
     vector<char> vect{'C','O','J','O','G'};
@@ -18,6 +18,7 @@ int main(){
     if (comp==true){cout<<"Complete"<<endl;}
     else {cout<<"Not complete"<<endl;}
     string name="Joueur A";
+    //Test Joueur
     Joueur joueur(name, *vit); //creation de Joueur
     joueur--;
     cout<<joueur<<endl; //testing operator <<
@@ -32,29 +33,31 @@ int main(){
     cout<<lots;
     lots.reset_L();
     cout<<lots;
-    /*
+    */
+
     int nColonne = 7;               //valeur par defaut de colonne du vitrail
     int nVitre = 5;                 // valeur par defaut du nombre de vitre par colonne
-    Vitrail* vitre=new Vitrail(nColonne, nVitre);     //initialize la grille de vitrail pour jouer
-    cout<< vitre <<endl;
-
+    Vitrail* vitre=new Vitrail(nColonne, nVitre);     //initialize la grille de vitrail pour jouer (2 joueuers utilisent le meme)
+    //cout<< vitre <<endl;
+    Lots lots; //faut aussi creer des Lots au debut (joueurs prennent du meme)
      //demande a utilisateur le nombre de vitre et colonne sinon defaut
     string name;
-    cout<< "Entree le nom du permier joueur: ";
+    cout<< "Entrez le nom du premier joueur: ";
     cin>> name;
-    Joueur* joueur1 = new Joueur(name, *vitre );      //Lots de vitre du joueur 1
-
-    cout<< "Entree le nom du deuxieme joueur: ";
+    Joueur joueur1(name, *vitre );      //Lots de vitre du joueur 1
+    cout<<joueur1<<endl;
+    cout<< "Entrez le nom du deuxieme joueur: ";
     cin>> name;
-    Joueur* joueur2 = new Joueur(name, *vitre);      //Lots de vitre du joueur 2
-
+    Joueur joueur2(name, *vitre);      //Lots de vitre du joueur 2
+    cout<<joueur2<<endl;
+    cout<<lots;
     bool gameflag = true;   //condition vrais pour continuer a jouer - false jeu terminer
-/*
+
     while(gameflag){
 
     }
 
-*/
+
 
     return 0;
 
